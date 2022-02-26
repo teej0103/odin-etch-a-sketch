a86611298d8c4ed3eea3e7534780b74046037a39
tj <tjciccone28@gmail.com>
2022-02-25 19:55:07 -0500

    game screen/type centered and initla board created

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hello friend");
+let gridContainer = document.querySelector('.grid-container');
+
+window.addEventListener('DOMContentLoaded', createGrid)
+
+function createGrid() {
+    let size = 16;
+    
+    for(let i = 0; i < Math.pow(size, 2); i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridContainer.appendChild(cell);
+    }
+
+    gridContainer.style.gridTemplateColumns = (`repeat(${size}, 1fr)`);
+    /*gridContainer.style.gridTemplateRows = (`repeat(${size}, 1fr)`);*/
+}
