ef0a4aba039787592b64e8bfd87c53a08a504e60
tj <tjciccone28@gmail.com>
2022-02-25 22:18:35 -0500

    added slider input for choosing grid size, need to figure out when to create a new grid, currently broken

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+// creates a 16x16 grid when page is initially loaded
 let gridContainer = document.querySelector('.grid-container');
+window.addEventListener('DOMContentLoaded', createGrid);
 
-window.addEventListener('DOMContentLoaded', createGrid)
+//gets size of grid from range slider input
+let x = document.querySelector('#grid-size-selector');
+x.addEventListener('input', displayGridSize)
+
+function displayGridSize(e) {
+    let div = document.querySelector('#grid-size-display');
+    div.innerText = `${e.target.value}x${e.target.value}`;
+    createGrid();
+}
 
 function createGrid() {
-    let size = 16;
+    let size = x.value;
     
     for(let i = 0; i < Math.pow(size, 2); i++) {
         let cell = document.createElement('div');
@@ -12,5 +22,15 @@ function createGrid() {
     }
 
     gridContainer.style.gridTemplateColumns = (`repeat(${size}, 1fr)`);
-    /*gridContainer.style.gridTemplateRows = (`repeat(${size}, 1fr)`);*/
 }
+
+function deleteGrid() {
+    gridContainer.innerHTML = '';
+}
+
+// GRID IS NOT RESETTING ITSELF AFTER ONE IS CREATED...
+// NEED TO ADD A BUTTON TO CONFIRM SIZE BEFORE PLAY, !!THEN!! CREATE THE GRID 
+// SO THEY DONT BUILD ON TOP OF EACHOTHER.
+// A RESET BUTTON WILL WIPE CLEAN, BUT THE PLAY BUTTON EVENT WILL BE WHEN THE GRID IS CREATED,
+// !!NOT!! EVERY TIME THE SLIDER CHANGES. THE SLIDER WILL STAY !!ONLY!! TO CONFIRM THE SIZE FOR THE PLAYER
+// BEFORE THEY PRESS PLAY BUTTON
